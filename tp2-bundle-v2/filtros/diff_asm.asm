15b36c1bcc438ac43fefb9f306425ab8d33edbfa
lew axel ariel <alew@ws6.labo4>
2015-09-14 16:28:50 -0300

    empezando el 1

--- tp2-bundle-v2/filtros/diff_asm.asm
+++ tp2-bundle-v2/filtros/diff_asm.asm
@@ -4,8 +4,8 @@ global diff_asm
 
 
 section .data
-
-
+	permu1: DB 3,1,2,4,3,1,2,4,3,1,2,4,3,1,2,4
+	permu2: DB 2,3,1,4,2,3,1,4,2,3,1,4,2,3,1,4
 
 section .text
 ;void diff_asm    (
@@ -17,6 +17,37 @@ section .text
 
 _diff_asm:
 diff_asm:
+	
+	; R12 = imagen1
+	; R13 = imagen2
+	; R14 = res
+	; R15 = filas
+	; RBX = cols
 
-
+	xor rcx,rcx
+	;cvtsi2sd xmm0, r15
+	;cvtsi2sd xmm1, rbx
+	;mulsd xmm0, xmm1
+	mul rbx, r15
+	mov rcx, rbx
+	.ciclo:
+		movAlgo xmm0, [r12]
+		movAlgo xmm1, [r13]
+		add r12, 16
+		add r13, 16
+		psubb xmm0, xmm1
+		movAlgo xmm2, [permu1]
+		movAlgo xmm3, [permu2]
+		mov xmm1, xmm0
+		shuffleAlgo xmm0, xmm2
+		pcmpgtb xmm0, xmm1
+		mov xmm3, xmm0
+		mov xmm0, xmm1
+		shuffleAlgo xmm0, xmm3
+		pcmpgtb xmm0, xmm1
+		and xmm0, xmm3
+		shuffleAlgo xmm1, xmm0
+		movAlgo [R14], xmm1
+		add r14, 16
+		loop
     ret
